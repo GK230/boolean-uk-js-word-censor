0ced0d241b3576933bea243e12640a1e7f54adc4
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-04-30 14:30:34 +0100

    completed challenges

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ const text =
   sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 function censor(text, replacement, toCensor) {
-  censored = []
+  let wordsCensored = 0;
+  let censored = []
   text = text.split(" ")
   for (word of text) {
     if (word === toCensor) {
       censored.push(replacement.repeat(toCensor.length))
+      wordsCensored++
     } else {
       censored.push(word)
     }
   }
   censored = censored.join(" ")
-  return text
+  console.log(censored)
+  console.log(wordsCensored)
+  return text, wordsCensored
 }
 
 censor(text, "!", "in")
